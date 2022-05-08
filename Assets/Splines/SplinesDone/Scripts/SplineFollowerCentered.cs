0a0ccc58a30d54d6a8ac9e0120dc1e1f7804f9d1
Syafiqjos <syafiqjos@gmail.com>
2022-05-08 21:57:38 +0700

    Add new custom spline follower scripts

---
+++ Assets/Splines/SplinesDone/Scripts/SplineFollowerCentered.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplineFollowerCentered : MonoBehaviour {
+    [SerializeField] private SplineFollowerManager manager;
+    [SerializeField] private int offsetIndex;
+
+    private void Update() {
+        float moveAmount = (manager.GetOffsetInitial() + offsetIndex * manager.GetOffsetInterval() + manager.GetMoveAmount()) % manager.GetMaxMoveAmount();
+
+        SplineDone spline = manager.GetSpline();
+
+        switch (manager.GetMovementType()) {
+            default:
+            case SplineFollowerManager.MovementType.Normalized:
+                transform.position = spline.GetPositionAt(moveAmount);
+                transform.forward = spline.GetForwardAt(moveAmount);
+                break;
+            case SplineFollowerManager.MovementType.Units:
+                transform.position = spline.GetPositionAtUnits(moveAmount);
+                transform.forward = spline.GetForwardAtUnits(moveAmount);
+                break;
+        }
+    }
+}
---
+++ Assets/Splines/SplinesDone/Scripts/SplineFollowerManager.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplineFollowerManager : MonoBehaviour
+{
+    public enum MovementType
+    {
+        Normalized,
+        Units
+    }
+
+    [SerializeField] private bool isRunning = true;
+
+    [SerializeField] private SplineDone spline;
+    [SerializeField] private float speed = 1f;
+    [SerializeField] private MovementType movementType;
+
+    [SerializeField] private float offsetInitial;
+    [SerializeField] private float offsetInterval;
+
+    private float maxMoveAmount;
+    private float moveAmount;
+
+    private void Start()
+    {
+        switch (movementType)
+        {
+            default:
+            case MovementType.Normalized:
+                maxMoveAmount = 1f;
+                break;
+            case MovementType.Units:
+                maxMoveAmount = spline.GetSplineLength();
+                break;
+        }
+    }
+
+    private void Update()
+    {
+        if (isRunning)
+        {
+            moveAmount = (moveAmount + (Time.deltaTime * speed));
+        }
+    }
+
+    public SplineDone GetSpline() {
+        return spline;
+    }
+
+    public float GetSpeed()
+    {
+        return speed;
+    }
+
+    public MovementType GetMovementType()
+    {
+        return movementType;
+    }
+
+    public float GetOffsetInitial()
+    {
+        return offsetInitial;
+    }
+
+    public float GetOffsetInterval()
+    {
+        return offsetInterval;
+    }
+
+    public float GetMaxMoveAmount()
+    {
+        return maxMoveAmount;
+    }
+
+    public float GetMoveAmount()
+    {
+        return moveAmount;
+    }
+}
