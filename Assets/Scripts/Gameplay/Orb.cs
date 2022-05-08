b3a1ba0b07f25bec27de71592e40c3f627e815f1
Syafiqjos <syafiqjos@gmail.com>
2022-05-08 23:18:22 +0700

    Add and implement orb generator to level

---
+++ Assets/Scripts/Gameplay/Orb.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orb : MonoBehaviour
+{
+    [SerializeField] private SplineFollowerCentered splineFollower;
+
+    public void SetIndex(int index)
+    {
+        splineFollower.SetOffsetIndex(index);
+    }
+
+    public void SetManager(SplineFollowerManager manager)
+    {
+        splineFollower.SetManager(manager);
+    }
+
+    public SplineFollowerCentered GetFollower()
+    {
+        return splineFollower;
+    }
+}
---
+++ Assets/Scripts/Gameplay/OrbGenerator.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrbGenerator : MonoBehaviour
+{
+    [SerializeField] private Orb orbPrefab;
+    [SerializeField] private SplineFollowerManager followerManager;
+    [SerializeField] private Transform orbsContainer;
+
+    private Orb foremostOrb = null;
+    private Orb latestOrb = null;
+
+    private float latestMoveAmount = 0;
+
+    private void Update()
+    {
+        CheckShouldAddOrb();
+    }
+
+    private void CheckShouldAddOrb()
+    {
+        // if pertama kali ngespawn
+        if (foremostOrb == null && latestOrb == null)
+        {
+            AddOrb();
+        } else
+        {
+            if (followerManager.GetMoveAmount() > latestMoveAmount + followerManager.GetOffsetInterval())
+            {
+                AddOrb();
+                latestMoveAmount = followerManager.GetMoveAmount();
+            }
+        }
+    }
+
+    public void AddOrb()
+    {
+        // if pertama kali ngespawn
+        if (foremostOrb == null && latestOrb == null)
+        {
+            Orb newOrb = SpawnOrb(0);
+            foremostOrb = newOrb;
+            latestOrb = newOrb;
+        } else
+        {
+            int index = latestOrb.GetFollower().GetOffsetIndex();
+            int newIndex = index + 1;
+
+            Orb newOrb = SpawnOrb(newIndex);
+            latestOrb = newOrb;
+        }
+    }
+
+    public Orb SpawnOrb(int index)
+    {
+        GameObject ne = Instantiate(orbPrefab.gameObject, orbsContainer);
+        Orb orb = ne.GetComponent<Orb>();
+
+        orb.SetManager(followerManager);
+        orb.SetIndex(index);
+
+        return orb;
+    }
+}
