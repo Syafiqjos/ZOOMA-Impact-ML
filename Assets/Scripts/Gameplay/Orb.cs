120e4501becc5ab06c69caab911a2b3fc5c05fc8
Syafiqjos <syafiqjos@gmail.com>
2022-05-09 17:47:18 +0700

    Add set index force and getter setter

--- Assets/Scripts/Gameplay/Orb.cs
+++ Assets/Scripts/Gameplay/Orb.cs
@@ -4,6 +4,20 @@ using System.Collections;
 public class Orb : MonoBehaviour
 {
     [SerializeField] private SplineFollowerCentered splineFollower;
+    [SerializeField] private OrbGenerator generator;
+
+    private Orb nextOrb; // foremost
+    private Orb prevOrb; // latest
+
+    public int GetIndex()
+    {
+        return splineFollower.GetOffsetIndex();
+    }
+
+    public void SetIndexForce(int index)
+    {
+        splineFollower.SetOffsetIndexForce(index);
+    }
 
     public void SetIndex(int index)
     {
@@ -15,8 +29,49 @@ public class Orb : MonoBehaviour
         splineFollower.SetManager(manager);
     }
 
+    public void SetGenerator(OrbGenerator generator)
+    {
+        this.generator = generator;
+    }
+
     public SplineFollowerCentered GetFollower()
     {
         return splineFollower;
+    }
+
+    public Orb GetNextOrb()
+    {
+        return nextOrb;
+    }
+
+    public void SetNextOrb(Orb orb)
+    {
+        nextOrb = orb;
+    }
+
+    public void SetPrevOrb(Orb orb)
+    {
+        prevOrb = orb;
+    }
+
+    public Orb GetPrevOrb()
+    {
+        return prevOrb;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Bullet")
+        {
+            ZumaBullet bullet = other.GetComponent<ZumaBullet>();
+            if (bullet && !bullet.GetPlacedFlag()) {
+                generator.PlaceNextOrb(this);
+                if (bullet)
+                {
+                    bullet.SetPlacedFlag(true);
+                }
+            }
+            Destroy(other.gameObject);
+        }
     }
 }
