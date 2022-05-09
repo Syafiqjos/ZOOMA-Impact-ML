c5b95b19de01fad65d90ef4bf375c64746c8f882
Syafiqjos <syafiqjos@gmail.com>
2022-05-09 17:47:53 +0700

    Add placed flag getter setter on zumma bullet

--- Assets/Scripts/Gameplay/ZumaBullet.cs
+++ Assets/Scripts/Gameplay/ZumaBullet.cs
@@ -6,8 +6,20 @@ public class ZumaBullet : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
 
+    private bool placedFlag = false;
+
     public void LaunchBullet(Vector3 direction, float speed)
     {
         rb.AddForce(direction * speed, ForceMode.Impulse);
+    }
+
+    public bool GetPlacedFlag()
+    {
+        return placedFlag;
+    }
+
+    public void SetPlacedFlag(bool flag)
+    {
+        placedFlag = flag;
     }
 }
